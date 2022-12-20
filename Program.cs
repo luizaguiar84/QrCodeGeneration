cc98f6baab27e86aa604fe89093cfa6fc56306d5
Luiz Aguiar <luizmanoel00@gmail.com>
2022-12-19 22:03:56 -0300

    fix

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using QrCodeGenerator;
+
+try
+{
+    /*
+    string texto = args[0];
+    string caminho = args[1];
+    int largura = int.Parse(args[2]);
+    int altura = int.Parse(args[2]);
+    */
+
+    string texto = "teste";
+    string caminho = @"C:\Development\QrCodeGenerator\teste1.bmp";
+    int largura = 150;
+    int altura = 150;
+    
+    var dir = Path.GetDirectoryName(caminho);
+    if (!Directory.Exists(dir))
+        Directory.CreateDirectory(dir);
+
+    texto = texto.Replace('#', ' ');
+    var qrCode = Service.GerarQRCodeDataMatrix(texto, largura, altura);
+    
+    Service.SaveBitmap(caminho, 100,100,qrCode);
+    
+}
+catch (Exception ex)
+{
+    throw new Exception($"Erro na criação do QrCode - {ex.Message}");
+}
--- Service.cs
+++ Service.cs
@@ -1,6 +1,46 @@
+using System.Drawing;
+using ZXing.QrCode;
+
 namespace QrCodeGenerator;
 
-public class Service
+public static class Service
 {
-    
+    public static byte[] GerarQRCodeDataMatrix(string text, int largura, int altura)
+    {
+        Byte[] byteArray;
+        var qrCodeWriter = new ZXing.BarcodeWriterPixelData
+        {
+            Format = ZXing.BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = altura,
+                Width = largura
+            }
+        };
+        var pixelData = qrCodeWriter.Write(text);
+
+        using var bitmap = new Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+        
+        using var ms = new MemoryStream();
+        var bitmapData = bitmap.LockBits(new Rectangle(0, 0, pixelData.Width, pixelData.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+        try
+        {
+            System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+        }
+        finally
+        {
+            bitmap.UnlockBits(bitmapData);
+        }
+        // save to stream as PNG
+        bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+        byteArray = ms.ToArray();
+        return byteArray;
+    }
+   public static void SaveBitmap(string fileName, int width, int height, byte[] imageData)
+   {
+       // Convert back to image.
+       using var ms = new MemoryStream(imageData);
+       var image = Image.FromStream(ms);
+       image.Save(fileName);
+   }
 }
